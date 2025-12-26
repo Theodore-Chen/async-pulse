a0c1a6d253b2b9a60c5eab601e814886f570ceb4
Theodore-Chen <qinchen.zhang@qq.com>
2025-12-27 03:14:40 +0800

    Update lock_free_bounded_queue

--- src/queue/lock_free_bounded_queue.h
+++ src/queue/lock_free_bounded_queue.h
@@ -56,8 +56,43 @@ class lock_free_bounded_queue {
     };
 
    public:
-    lock_free_bounded_queue(size_t capacity) {}
+    lock_free_bounded_queue(size_t capacity) : buffer_(capacity), buffer_mask_(capacity - 1) {
+        for (size_t i = 1; i < capacity; i++) {
+            buffer_[i].sequence.store(i, std::memory_order::memory_order_relaxed);
+        }
+        pos_enqueue.store(0, std::memory_order::memory_order_relaxed);
+        pos_dequeue.store(0, std::memory_order::memory_order_relaxed);
+    }
+    ~lock_free_bounded_queue() {}
+
+    lock_free_bounded_queue(const lock_free_bounded_queue&) = delete;
+    lock_free_bounded_queue(lock_free_bounded_queue&&) = delete;
+    lock_free_bounded_queue& operator=(const lock_free_bounded_queue&) = delete;
+    lock_free_bounded_queue& operator=(lock_free_bounded_queue&&) = delete;
+
+    bool enqueue(const value_type& val) {
+        for (;;) {
+            size_t pos = pos_enqueue.load();
+            cell_type* cell = buffer_[pos & buffer_mask_];
+            size_t seq = cell->sequence.load();
+            int64_t dif = static_cast<int64_t>(pos) - static_cast<int64_t>(seq);
+            if (dif == 0) {
+                if (pos_enqueue.compare_exchange_weak(pos, pos + 1)) {
+                    break;
+                }
+            } else if (dif < 0) {
+                if (pos - pos_dequeue.load() == capacity())
+            }
+        }
+    }
+
+    size_t capacity() {
+        return buffer_.capacity();
+    }
 
    private:
-    uninitialized_buffer<value_type>* buffer_;
+    uninitialized_buffer<value_type> buffer_;
+    const size_t buffer_mask_;
+    sequence_type pos_enqueue;
+    sequence_type pos_dequeue;
 };
